538aae69dc046b3b461dfe5e3dc637f4b7192eb4
Almog Zim <almogzt@gmail.com>
2019-11-16 15:14:24 +0200

    version 0.1.1 release, see README for changes

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Google Chrome extension that helps to see which product is shipping free to Israel.
 
 See here in Google Chrome Store: https://cutt.ly/AmazonILFSM
+
+
+
+Updates:
+
+v0.1 - 	initial release
+
+v0.1.1 - 
+- Fixed issue when the extension isn't loaded on page when you do a refresh
+- Added support in cart page for products which are dynamic loaded
+- Added support in cart page when you move product from cart to 'saved for later' and vice versa
+- Added support in product page when you can select different product option/color
--- src/code/scripts.js
+++ src/code/scripts.js
@@ -1,29 +1,126 @@
+var shippingMarker,
+	extensionRan = false,
+	debugMode = false;
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "interactive") {
 		clearInterval(readyStateCheckInterval);
 
-	$( document ).ready(function() {
-		var styleClass = 'amazonFreeShipping';
-
-		try {
-			// category / results page
-			$('div.s-align-children-center:contains("FREE Shipping to Israel")').parent().parent().parent().parent().parent().addClass(styleClass);
-
-			// cart page
-			$('div.sc-list-item-content:contains("FREE Shipping to Israel")').addClass(styleClass);
-
-			// product page
-			$('div.a-box-inner:contains("(Eligible for FREE Shipping on qualifying orders over $49.00)")').addClass(styleClass).find('.accordion-row-content').addClass(styleClass);
-			$('div#price:contains("(Eligible for FREE Shipping on qualifying orders over $49.00)")').addClass(styleClass).find('.accordion-row-content').addClass(styleClass);
-			
-			$('div.a-box-inner:contains("FREE Shipping to Israel")').addClass(styleClass).find('.accordion-row-content').addClass(styleClass);
-			$('div#price:contains("FREE Shipping to Israel")').addClass(styleClass).find('.accordion-row-content').addClass(styleClass);
-		} catch(e) {
-			
-		}
-	});
-
+		$( document ).ready(function() {
+			shippingMarker = new amazonShippingMarker(debugMode);
+			extensionRan = true;
+		});
 	}
 	}, 10);
 });
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		if(!extensionRan) {
+			$( document ).ready(function() {
+				shippingMarker = new amazonShippingMarker(debugMode);
+				extensionRan = true;
+			});
+		}
+	}
+	}, 10);
+});
+
+
+
+
+function amazonShippingMarker(debug = false) {
+	this.currentPage = location.href;
+	this.styleClass = 'amazonFreeShipping';
+	this.inCart = (this.currentPage.indexOf('/cart/')!=-1) ? true : false;
+	this.inProduct = (this.currentPage.indexOf('/product/')!=-1 || this.currentPage.indexOf('/dp/')!=-1) ? true : false;
+	this.inCatalog = (!this.inCart && !this.inProduct) ? true : false;
+	this.debug = debug;
+    this.init();
+}
+
+amazonShippingMarker.prototype = {
+    init: function () {
+		if(this.debug) {
+			console.log('in cart: '+this.inCart);
+			console.log('in product: '+this.inProduct);
+			console.log('in catalog: '+this.inCatalog);
+		}
+
+		// Mark products based on current page
+		if(this.inCatalog) {
+			this.markCatalogProducts();
+		} else if(this.inProduct) {
+			this.markProductPage();
+			this.detectProductUrlChange();
+		} else if(this.inCart) {
+			this.markCartProducts();
+		}
+    },
+
+    markCatalogProducts: function () {
+
+		try {
+			$('div.s-align-children-center:contains("FREE Shipping to Israel")').parent().parent().parent().parent().parent().addClass(this.styleClass);
+
+			if(this.debug) console.log('marked products in catalog page');
+		} catch(e) {
+			
+		}
+    },
+
+    markCartProducts: function () {
+		
+		try {
+
+			var _this = this;
+			var markProductInCart = function() {
+				$('div.sc-list-item-content:contains("FREE Shipping to Israel")').addClass(_this.styleClass);
+			};
+			
+			markProductInCart();
+
+			$("#sc-saved-cart-items").bind("DOMSubtreeModified",function(){
+				markProductInCart();
+			});
+			
+			$(".sc-list-body").bind("DOMSubtreeModified",function(){
+				markProductInCart();
+			});
+
+			if(this.debug) console.log('marked products in cart page');
+		} catch(e) {
+			
+		}
+    },
+
+    markProductPage: function () {
+		
+		try {
+			$('div.a-box-inner:contains("(Eligible for FREE Shipping on qualifying orders over $49.00)")').addClass(this.styleClass).find('.accordion-row-content').addClass(this.styleClass);
+			$('div#price:contains("(Eligible for FREE Shipping on qualifying orders over $49.00)")').addClass(this.styleClass).find('.accordion-row-content').addClass(this.styleClass);
+			
+			$('div.a-box-inner:contains("FREE Shipping to Israel")').addClass(this.styleClass).find('.accordion-row-content').addClass(this.styleClass);
+			$('div#price:contains("FREE Shipping to Israel")').addClass(this.styleClass).find('.accordion-row-content').addClass(this.styleClass);
+
+			if(this.debug) console.log('marked products in product page');
+		} catch(e) {
+			
+		}
+	},
+	
+	detectProductUrlChange: function () {
+		var _this = this;
+		this.productInterval = setInterval(function() {
+			if(_this.currentPage != location.href) {
+				_this.currentPage = location.href;
+				_this.markProductPage();
+			}
+		}, 700);
+
+	}
+
+};
